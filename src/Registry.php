8175e9006e86c9c2bad24f0fedfecc61f6338008
Jian Liu <onanying@126.com>
2019-04-26 16:52:47 +0800

    ::$coroutineMode修改为::COROUTINE_MODE

--- src/Registry.php
+++ src/Registry.php
@@ -18,7 +18,7 @@ class Registry extends AbstractComponent
      * 协程模式
      * @var int
      */
-    public static $coroutineMode = ComponentInterface::COROUTINE_MODE_REFERENCE;
+    const COROUTINE_MODE = ComponentInterface::COROUTINE_MODE_REFERENCE;
 
     /**
      * 处理者
